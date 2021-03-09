9f69ac2fc7b971050f5c053f7a5a1842815cd64f
pmzaverukha <pm.zaverukha@gmail.com>
2021-03-09 23:36:52 +0200

    feat: add function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    // return Math.min.apply(0, array);
+    if (Array.isArray(array) && array.length)
+        return Math.min.apply(0, array);
+    else
+        return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length)
+        return Math.max.apply(null, array);
+    else
+        return 0;
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length)
+        return array.reduce((a, b) => (a + b)) / array.length;
+    else
+        return 0;
+
 }
